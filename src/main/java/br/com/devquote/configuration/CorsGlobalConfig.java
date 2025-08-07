3c2f576a89a22e2c79648a4a2ade7a52fd5045b6
Wesley Eduardo <wesleyeduardo.dev@gmail.com>
2025-08-07 08:33:37 -0300

    ajuste no cors

--- src/main/java/br/com/devquote/configuration/CorsGlobalConfig.java
+++ src/main/java/br/com/devquote/configuration/CorsGlobalConfig.java
@@ -13,7 +13,7 @@ public class CorsGlobalConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173")
+                        .allowedOrigins("http://localhost:3000")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
