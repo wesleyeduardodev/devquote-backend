11f38ca13243a7de3a182838069dc71fff33860b
Wesley Eduardo <111645247+wesleyeduardodev@users.noreply.github.com>
2025-09-06 20:30:03 -0300

    sdfg

--- src/main/java/br/com/devquote/configuration/WebMvcConfig.java
+++ src/main/java/br/com/devquote/configuration/WebMvcConfig.java
@@ -1,15 +1,14 @@
 package br.com.devquote.configuration;
-
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@RequiredArgsConstructor
 public class WebMvcConfig implements WebMvcConfigurer {
 
-    @Autowired
-    private LoggingInterceptor loggingInterceptor;
+    private final LoggingInterceptor loggingInterceptor;
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
