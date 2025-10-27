ee4ad7129ad5a0e14dafc6be6a9b1934710aad67
Wesley Eduardo <wesleyeduardo.dev@gmail.com>
2025-10-27 18:30:14 -0300

    chore: bump API version to v4 - trigger Docker rebuild with Redis support

--- src/main/java/br/com/devquote/configuration/openapi/OpenApiConfig.java
+++ src/main/java/br/com/devquote/configuration/openapi/OpenApiConfig.java
@@ -21,7 +21,7 @@ public class OpenApiConfig {
                 .info(new Info()
                         .title("DevQuote API")
                         .description("Sistema de orçamentos automatizados.\n\n" +
-                                "📅 Data da subida: 27/10/2025 v3\n" +
+                                "📅 Data da subida: 27/10/2025 v4\n" +
                                 "🔖 Versão: 1.0.2\n" +
                                 "👨‍💻 Autor: Wesley Eduardo\n")
                         .version("1.0.0")
