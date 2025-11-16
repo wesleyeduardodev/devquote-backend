f4e026a01197521508cee9c58f53ba6a74fda223
Wesley Eduardo <wesleyeduardo.dev@gmail.com>
2025-11-16 10:26:12 -0300

    configuracao de horarios

--- src/main/java/br/com/devquote/DevquoteApplication.java
+++ src/main/java/br/com/devquote/DevquoteApplication.java
@@ -1,9 +1,16 @@
 package br.com.devquote;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import jakarta.annotation.PostConstruct;
+import java.util.TimeZone;
 
 @SpringBootApplication
 public class DevquoteApplication {
+
+	@PostConstruct
+	public void init() {
+		TimeZone.setDefault(TimeZone.getTimeZone("America/Sao_Paulo"));
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(DevquoteApplication.class, args);
