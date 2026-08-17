3aad3b667775861f6799a13e8e2c4b17af677fe2
Wesley Eduardo <111645247+wesleyeduardodev@users.noreply.github.com>
2026-08-17 10:26:03 -0300

    17/08/2026

--- src/main/java/br/com/devquote/configuration/openapi/OpenApiConfig.java
+++ src/main/java/br/com/devquote/configuration/openapi/OpenApiConfig.java
@@ -21,7 +21,7 @@ public class OpenApiConfig {
                 .info(new Info()
                         .title("DevQuote API")
                         .description("Sistema de orçamentos automatizados.\n\n" +
-                                "📅 Data da subida: 01/02/2026\n" +
+                                "📅 Data da subida: 17/08/2026\n" +
                                 "🔖 Versão: 1.0.2\n" +
                                 "👨‍💻 Autor: Wesley Eduardo\n")
                         .version("1.0.0")
